91dff6407c351f925102459a44a18618c8b54217
Johannes Nicolai <jonico@github.com>
2021-02-12 15:38:55 +0100

    luck

--- bindings/python/samples/pod-status-led.py
+++ bindings/python/samples/pod-status-led.py
@@ -113,31 +113,11 @@ class PodStatusLed(SampleBase):
                 for x in range (0, podPixelLength):
                     for y in range (0, podPixelHeight):
                         print("x: %d, y: %d, color: %s" % (basePosX + offsetX + x, basePosY + y, status_color(pod.podStatus)))
+                        self.matrix.SetPixel(basePosX + offsetX + x, basePosY + y, status_color_led(pod.podStatus))
                 i+=1
             offsetX += maxX
 
-        sub_blocks = 16
-        width = self.matrix.width
-        height = self.matrix.height
-        x_step = max(1, width / sub_blocks)
-        y_step = max(1, height / sub_blocks)
-        count = 0
-
-        while True:
-            for y in range(0, height):
-                for x in range(0, width):
-                    c = sub_blocks * int(y / y_step) + int(x / x_step)
-                    if count % 4 == 0:
-                        self.matrix.SetPixel(x, y, c, c, c)
-                    elif count % 4 == 1:
-                        self.matrix.SetPixel(x, y, c, 0, 0)
-                    elif count % 4 == 2:
-                        self.matrix.SetPixel(x, y, 0, c, 0)
-                    elif count % 4 == 3:
-                        self.matrix.SetPixel(x, y, 0, 0, c)
-
-            count += 1
-            time.sleep(2)
+            time.sleep(20)
 
 
 # Main function
