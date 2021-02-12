abc27e25efcab192981df125e38a5ffd0f3f7f45
Johannes Nicolai <jonico@github.com>
2021-02-12 15:51:02 +0100

    loop

--- bindings/python/samples/pod-status-led.py
+++ bindings/python/samples/pod-status-led.py
@@ -53,73 +53,74 @@ class PodStatusLed(SampleBase):
         podPixelHeight=8
         positionMax = (maxX/podPixelLength)*(maxY/podPixelHeight)
 
-        podsSeenThisRound = set()
-        podsToBeInsertedThisRound = []
+        while True:
 
-        output = subprocess.getoutput("kubectl get pods --namespace actions-runner-link --no-headers -o wide")
-        for row in output.split("\n"):
-            values = row.split();
-            if (not values):
-                continue
-            podName = values[0]
-            podStatus = values[2]
-            nodeName = values[6]
+            podsSeenThisRound = set()
+            podsToBeInsertedThisRound = []
 
-            if (nodeName not in nodes.keys()):
-                continue
+            output = subprocess.getoutput("kubectl get pods --namespace actions-runner-link --no-headers -o wide")
+            for row in output.split("\n"):
+                values = row.split();
+                if (not values):
+                    continue
+                podName = values[0]
+                podStatus = values[2]
+                nodeName = values[6]
 
-            podsSeenThisRound.add(podName)
+                if (nodeName not in nodes.keys()):
+                    continue
 
-            pod = nodes.get(nodeName).get(podName)
-            if (not pod):
-                # we have to schedule the position after this lopp
-                podsToBeInsertedThisRound.append(Pod(podName, podStatus, nodeName, -1))
-            else:
-                # we only change the status, position is already set
-                nodes[nodeName][podName] = Pod(podName, podStatus, nodeName, pod.position)
+                podsSeenThisRound.add(podName)
 
-        performedDefrag = False
-        for pod in podsToBeInsertedThisRound:
-            position = find_first_unused_position(positionsAlreadyTaken[pod.podNode])
-            if position >= positionMax:
-                if not performedDefrag:
-                    # idea: turn defrag logic into a function
-                    for node, existingPods in nodes.items():
-                        for podName, existingPod in existingPods.items():
-                            if (not podName in podsSeenThisRound):
-                                # mark position for potential override, don't do it yet
-                                positionsAlreadyTaken[existingPod.podNode].remove(existingPod.position)
-                    performedDefrag = True
+                pod = nodes.get(nodeName).get(podName)
+                if (not pod):
+                    # we have to schedule the position after this lopp
+                    podsToBeInsertedThisRound.append(Pod(podName, podStatus, nodeName, -1))
+                else:
+                    # we only change the status, position is already set
+                    nodes[nodeName][podName] = Pod(podName, podStatus, nodeName, pod.position)
+
+            performedDefrag = False
+            for pod in podsToBeInsertedThisRound:
                 position = find_first_unused_position(positionsAlreadyTaken[pod.podNode])
+                if position >= positionMax:
+                    if not performedDefrag:
+                        # idea: turn defrag logic into a function
+                        for node, existingPods in nodes.items():
+                            for podName, existingPod in existingPods.items():
+                                if (not podName in podsSeenThisRound):
+                                    # mark position for potential override, don't do it yet
+                                    positionsAlreadyTaken[existingPod.podNode].remove(existingPod.position)
+                        performedDefrag = True
+                    position = find_first_unused_position(positionsAlreadyTaken[pod.podNode])
 
-            pod.position = position
-            positionsAlreadyTaken[pod.podNode].add(position)
-            nodes[pod.podNode][pod.podName] = pod
-            if (position<len(nodesByPosition[pod.podNode])):
-                previousPod = nodesByPosition[pod.podNode][pod.position]
-                nodes[previousPod.podNode].pop(previousPod.podName)
-                nodesByPosition[pod.podNode][pod.position]=pod
-            else:
-                nodesByPosition[pod.podNode].append(pod)
+                pod.position = position
+                positionsAlreadyTaken[pod.podNode].add(position)
+                nodes[pod.podNode][pod.podName] = pod
+                if (position<len(nodesByPosition[pod.podNode])):
+                    previousPod = nodesByPosition[pod.podNode][pod.position]
+                    nodes[previousPod.podNode].pop(previousPod.podName)
+                    nodesByPosition[pod.podNode][pod.position]=pod
+                else:
+                    nodesByPosition[pod.podNode].append(pod)
 
 
-        offsetX = 0
-        for node, pods in nodesByPosition.items():
-            i = 0
-            for pod in pods:
-                print("Pod: %s, Status: %s, Node: %s, Color: %s, Position: %i" % (pod.podName, pod.podStatus, pod.podNode, status_color(pod.podStatus), pod.position))
-                basePosX = (i * podPixelLength) % maxX
-                print ("BasePos: %d" % basePosX)
-                basePosY = (int) (i*podPixelLength/maxX) * podPixelHeight
-                for x in range (0, podPixelLength):
-                    for y in range (0, podPixelHeight):
-                        print("x: %d, y: %d, color: %s" % (basePosX + offsetX + x, basePosY + y, status_color(pod.podStatus)))
-                        color = status_color_led(pod.podStatus)
-                        self.matrix.SetPixel(basePosX + offsetX + x, basePosY + y, color.red, color.green, color.blue)
-                i+=1
-            offsetX += maxX
-
-        time.sleep(20)
+            offsetX = 0
+            for node, pods in nodesByPosition.items():
+                i = 0
+                for pod in pods:
+                    print("Pod: %s, Status: %s, Node: %s, Color: %s, Position: %i" % (pod.podName, pod.podStatus, pod.podNode, status_color(pod.podStatus), pod.position))
+                    basePosX = (i * podPixelLength) % maxX
+                    print ("BasePos: %d" % basePosX)
+                    basePosY = (int) (i*podPixelLength/maxX) * podPixelHeight
+                    for x in range (0, podPixelLength):
+                        for y in range (0, podPixelHeight):
+                            print("x: %d, y: %d, color: %s" % (basePosX + offsetX + x, basePosY + y, status_color(pod.podStatus)))
+                            color = status_color_led(pod.podStatus)
+                            self.matrix.SetPixel(basePosX + offsetX + x, basePosY + y, color.red, color.green, color.blue)
+                    i+=1
+                offsetX += maxX
+            time.sleep(5)
 
 
 # Main function
